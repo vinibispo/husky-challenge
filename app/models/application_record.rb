98603b54a45367d23e5c4186702451e3228733c1
vinibispo <vini.bispo015@gmail.com>
2022-03-26 15:58:11 -0300

    chore(reek): make reek happy describing module application_record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# This class is responsible for integrate ActiveModel with database
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
   include AttrKeyring.active_record

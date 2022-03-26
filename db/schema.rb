09afe7caa9c00a8d5704547bcc6c391fcb95b8a2
vinibispo <vini.bispo015@gmail.com>
2022-03-26 10:27:56 -0300

    chore(schema): Update db schema
    o

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_163326) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_31_163326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,8 +21,8 @@ ActiveRecord::Schema.define(version: 2022_01_31_163326) do
     t.text "customer_notes"
     t.float "total_amount_due"
     t.string "emails"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

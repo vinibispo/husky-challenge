226cc4a5ebe886dbe78a92a60804a14a52ea47fe
vinibispo <vini.bispo015@gmail.com>
2022-04-02 14:26:43 -0300

    test(support): add support for writing system spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 
 require_relative 'support/factory_bot'
 require_relative 'support/shoulda_matchers'
+require_relative 'support/system_spec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

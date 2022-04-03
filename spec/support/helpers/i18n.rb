4b666b3c4f2528466d9eb03d0125e0885e86ee6c
vinibispo <vini.bispo015@gmail.com>
2022-04-03 17:46:50 -0300

    feat(i18n): add test helper i18n for flash messages

--- spec/support/helpers/i18n.rb
+++ spec/support/helpers/i18n.rb
@@ -9,4 +9,9 @@ module Helpers::I18n
   def button(scope)
     ::I18n.t(scope, scope: 'forms.buttons')
   end
+
+  # :reek:UtilityFunction
+  def flash(scope)
+  ::I18n.t(scope, scope: 'flash')
+  end
 end

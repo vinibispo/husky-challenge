61adcc716fb71ea37d2e566d4575708d81dec82a
vinibispo <vini.bispo015@gmail.com>
2022-04-03 13:52:25 -0300

    chore(i18n): add reek comment to ignore UtilityFunction

--- spec/support/helpers/i18n.rb
+++ spec/support/helpers/i18n.rb
@@ -1,9 +1,11 @@
 # spec helpers for i18n
 module Helpers::I18n
+  # :reek:UtilityFunction
   def field(scope)
     ::I18n.t(scope, scope: 'forms.labels')
   end
 
+  # :reek:UtilityFunction
   def button(scope)
     ::I18n.t(scope, scope: 'forms.buttons')
   end

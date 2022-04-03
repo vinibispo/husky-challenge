1f386e87d70ad261af2ec2da22eab7c9da25c6b4
vinibispo <vini.bispo015@gmail.com>
2022-04-03 12:31:53 -0300

    feat(i18n): add i18n helpers on rspec

--- spec/support/helper.rb
+++ spec/support/helper.rb
@@ -1,5 +1,7 @@
 require_relative 'helpers'
 require_relative 'helpers/authentication'
+require_relative 'helpers/i18n'
 RSpec.configure do |config|
   config.include Helpers::Authentication, type: :system
+  config.include Helpers::I18n
 end

229cf56aea9453da6d2ef36f5aad3c262e4367ce
vinibispo <vini.bispo015@gmail.com>
2022-04-03 17:57:25 -0300

    refactor(i18n): change flash messages words fixed using t method from i18n in controllers

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -6,9 +6,9 @@ class TokensController < ApplicationController
     if params[:token] == '123456'
       session[:current_user_token] = params[:token]
 
-      redirect_to(invoices_path, notice: "Token válido")
+      redirect_to(invoices_path, notice: I18n.t('tokens.create.success', scope: 'flash'))
     else
-      redirect_to(root_path, notice: "Token inválido")
+      redirect_to(root_path, notice: I18n.t('tokens.create.error', scope: 'flash'))
     end
   end
 end

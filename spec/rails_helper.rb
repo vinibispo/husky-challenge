ec3d6bac47304c5a02d159890d93a03e5e02fd93
vinibispo <vini.bispo015@gmail.com>
2022-04-08 08:41:40 -0300

    chore(rails_helper): add use_transaction_fixtures for running every example within a transaction

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.use_transactional_fixtures = true
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 

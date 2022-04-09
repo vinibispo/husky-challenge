5fa940986f1797296e1dbd534857c875e79759c7
vinibispo <vini.bispo015@gmail.com>
2022-04-09 16:22:23 -0300

    feat(form_helper): remove validation object#nil?

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,9 +1,7 @@
 # form helper for forms
 module FormHelper
-  # :reek:UtilityFunction
   def create_or_update(object)
-    return :submit if object.nil?
-    return 'update' if object.persisted?
+    return 'update' if object&.persisted?
 
     'create'
   end

37e88961a69cec72d0e3eb320c8e0d3f633421db
vinibispo <vini.bispo015@gmail.com>
2022-04-09 14:34:24 -0300

    refactor(controllers): refactor tokens_controller using "one action per controller"

---
+++ app/controllers/tokens/create_controller.rb
@@ -0,0 +1,12 @@
+# create controller for tokens
+class Tokens::CreateController < ApplicationController
+  def call
+    if params[:token] == '123456'
+      session[:current_user_token] = params[:token]
+
+      redirect_to(invoices_path, notice: I18n.t('tokens.create.success', scope: 'flash'))
+    else
+      redirect_to(root_path, notice: I18n.t('tokens.create.error', scope: 'flash'))
+    end
+  end
+end

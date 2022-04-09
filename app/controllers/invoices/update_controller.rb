f3fd66e769b8a4484ae9cb9ac3295a2c8c8fc2c9
vinibispo <vini.bispo015@gmail.com>
2022-04-09 14:21:36 -0300

    refactor(invoices_controller): convert invoices_controller in controllers like hanami way

---
+++ app/controllers/invoices/update_controller.rb
@@ -0,0 +1,25 @@
+# update controller for invoices
+class Invoices::UpdateController < ApplicationController
+  def call
+    return redirect_if_is_not_authenticated if not_authenticated_and_html?
+
+    @invoice = Invoice.find(params[:id])
+
+    respond_to do |format|
+      which_screen_should_render(format)
+    end
+  end
+
+  private
+
+
+  def which_screen_should_render(format)
+    if @invoice.update(Invoice::Params.to_save(params))
+      format.html { redirect_to invoice_url(@invoice), notice: I18n.t("flash.invoices.update.success") }
+      format.json { render 'invoices/show', status: :ok, location: @invoice }
+    else
+      format.html { render 'invoices/edit', status: :unprocessable_entity }
+      format.json { render json: @invoice.errors, status: :unprocessable_entity }
+    end
+  end
+end

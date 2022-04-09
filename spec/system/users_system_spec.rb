464376b53483409098cfe994d0a615e1ad6978fc
vinibispo <vini.bispo015@gmail.com>
2022-04-09 20:17:38 -0300

    test(users): add user system test fill_in with email

--- spec/system/users_system_spec.rb
+++ spec/system/users_system_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 RSpec.describe 'Invoices', type: :system do
-  it 'should render new' do
+  it 'should create a user (generate access token)' do
+    user = build(:user)
     visit '/users/new'
+
+    fill_in :email, with: user.email
   end
 end

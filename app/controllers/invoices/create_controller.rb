12ae8b4e7d830cf0df64d8d5a5f70b41327c3f32
vinibispo <vini.bispo015@gmail.com>
2022-04-09 15:14:39 -0300

    refactor(controllers): replace instance variables for local variables

--- app/controllers/invoices/create_controller.rb
+++ app/controllers/invoices/create_controller.rb
@@ -5,21 +5,21 @@ class Invoices::CreateController < ApplicationController
   def call
     return redirect_if_is_not_authenticated if not_authenticated_and_html?
 
-    @invoice = Invoice.new(Invoice::Params.to_save(params))
+    invoice = Invoice.new(Invoice::Params.to_save(params))
     respond_to do |format|
-      which_screen_should_render(format)
+      which_screen_should_render(format, invoice)
     end
   end
 
   private
 
-  def which_screen_should_render(format)
-    if @invoice.save
-      format.html { redirect_to invoice_url(@invoice), notice: I18n.t("flash.invoices.create.success") }
-      format.json { render 'invoices/show', status: :created, location: @invoice }
+  def which_screen_should_render(format, invoice)
+    if invoice.save
+      format.html { redirect_to invoice_url(invoice), notice: I18n.t("flash.invoices.create.success") }
+      format.json { render 'invoices/show', locals: { invoice: }, status: :created, location: invoice }
     else
-      format.html { render 'invoices/new', status: :unprocessable_entity }
-      format.json { render json: @invoice.errors, status: :unprocessable_entity }
+      format.html { render 'invoices/new', locals: { invoice: }, status: :unprocessable_entity }
+      format.json { render json: invoice.errors, status: :unprocessable_entity }
     end
   end
 end
--- app/controllers/invoices/update_controller.rb
+++ app/controllers/invoices/update_controller.rb
@@ -3,22 +3,22 @@ class Invoices::UpdateController < ApplicationController
   def call
     return redirect_if_is_not_authenticated if not_authenticated_and_html?
 
-    @invoice = Invoice.find(params[:id])
+    invoice = Invoice.find(params[:id])
 
     respond_to do |format|
-      which_screen_should_render(format)
+      which_screen_should_render(format, invoice)
     end
   end
 
   private
 
-  def which_screen_should_render(format)
-    if @invoice.update(Invoice::Params.to_save(params))
-      format.html { redirect_to invoice_url(@invoice), notice: I18n.t("flash.invoices.update.success") }
-      format.json { render 'invoices/show', status: :ok, location: @invoice }
+  def which_screen_should_render(format, invoice)
+    if invoice.update(Invoice::Params.to_save(params))
+      format.html { redirect_to invoice_url(invoice), notice: I18n.t("flash.invoices.update.success") }
+      format.json { render 'invoices/show', locals: { invoice: }, status: :ok, location: invoice }
     else
-      format.html { render 'invoices/edit', status: :unprocessable_entity }
-      format.json { render json: @invoice.errors, status: :unprocessable_entity }
+      format.html { render 'invoices/edit', locals: { invoice: }, status: :unprocessable_entity }
+      format.json { render json: invoice.errors, status: :unprocessable_entity }
     end
   end
 end

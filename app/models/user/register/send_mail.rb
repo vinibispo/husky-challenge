7df7c793f568d6f1851c7b715c8379043f17ebcb
vinibispo <vini.bispo015@gmail.com>
2022-04-10 17:05:27 -0300

    feat(send_mail): start implementation of send_mail

--- app/models/user/register/send_mail.rb
+++ app/models/user/register/send_mail.rb
@@ -1,4 +1,7 @@
 class User::Register::SendMail < Micro::Case
+  attribute :user
+  attribute :mailer, default: ->(value) { value || UserMailer }
   def call!
+    mailer.with(user: user).send_token.deliver_later
   end
 end

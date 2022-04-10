7a6300433a8527f1548bfadb7a765476c6c8b1bc
vinibispo <vini.bispo015@gmail.com>
2022-04-10 15:32:49 -0300

    feat(create): start implementation create

--- app/models/user/register/create.rb
+++ app/models/user/register/create.rb
@@ -1,6 +1,15 @@
 class User::Register::Create < Micro::Case
   attribute :user
-  validates :user, kind: User
+  attribute :email
+  validates :user, kind: { of: [User, NilClass] }
+  validates :email, kind: String
   def call!
+    return Success result: { user: } if user.present?
+
+    new_user = User.new(email:)
+
+    return Success result: { user: new_user } if new_user.save
+
+    Failure :invalid_email, result: { errors: new_user.errors }
   end
 end

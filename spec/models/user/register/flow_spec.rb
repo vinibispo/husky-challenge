db278d456f32a30c3c303ae4334dd6d60fd18c4f
vinibispo <vini.bispo015@gmail.com>
2022-04-10 18:06:32 -0300

    test(user): add test for regenerate token in flow

--- spec/models/user/register/flow_spec.rb
+++ spec/models/user/register/flow_spec.rb
@@ -3,9 +3,23 @@ RSpec.describe User::Register::Flow do
   it 'should get user params and return user created' do
     params = ActionController::Parameters.new({ user: { email: 'johndoe@husky.io' }})
 
-    result = described_class.call(params:)
+    result = User::Register::Flow.call(params:)
 
     user = User.last
+    expected = { user: }
+
+    expect(result.success?).to be true
+    expect(result.data).to eq(expected)
+  end
+
+  it 'should get user params when user exists and just regenerate token' do
+    email = 'johndoe@husky.io'
+    user = create(:user)
+    params = ActionController::Parameters.new({ user: { email: } })
+
+    result = User::Register::Flow.call(params:)
+    
+    user.reload
     expected = { user: }
 
     expect(result.success?).to be true

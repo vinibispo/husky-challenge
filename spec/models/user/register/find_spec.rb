03d975c61bb42f0d65d96275931f5b74653fed3f
vinibispo <vini.bispo015@gmail.com>
2022-04-10 11:52:04 -0300

    test(find): add test for find for register for user

--- spec/models/user/register/find_spec.rb
+++ spec/models/user/register/find_spec.rb
@@ -9,6 +9,17 @@ RSpec.describe User::Register::Find do
 
     expect(result.success?).to be true
     expect(result.data).to eq(expected)
+  end
 
+  it 'should return success where there is user' do
+    email = 'johndoe@husky.io'
+    user = create(:user, email:)
+
+    result = User::Register::Find.call(email:)
+
+    expected = { user:, email: }
+
+    expect(result.success?).to be true
+    expect(result.data).to eq(expected)
   end
 end

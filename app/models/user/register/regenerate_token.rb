1e8ccfe99774f6b208b9b0e158335fd336b0867d
vinibispo <vini.bispo015@gmail.com>
2022-04-10 14:44:48 -0300

    feat(regenerate_token): add real implementation for regenerate_token

--- app/models/user/register/regenerate_token.rb
+++ app/models/user/register/regenerate_token.rb
@@ -1,4 +1,12 @@
 class User::Register::RegenerateToken < Micro::Case
+  attribute :email
+  attribute :user
+
+  validates :email, kind: String
+  validates :user, kind: { of: [User, NilClass] }
+
   def call!
+    user.generate_token if user.present?
+    Success result: {email:, user:}
   end
 end

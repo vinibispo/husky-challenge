afe2fd2361454529ae413402f72dcabd8fbe577b
vinibispo <vini.bispo015@gmail.com>
2022-04-10 17:17:17 -0300

    fix(send_mail): finish implementation of send_mail

--- app/models/user/register/send_mail.rb
+++ app/models/user/register/send_mail.rb
@@ -1,7 +1,9 @@
 class User::Register::SendMail < Micro::Case
   attribute :user
   attribute :mailer, default: ->(value) { value || UserMailer }
+  validates :user, kind: User
   def call!
     mailer.with(user: user).send_token.deliver_later
+    Success result: { user: }
   end
 end

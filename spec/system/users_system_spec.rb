05981acae377217f4a78f86804242c7b2b88d05e
vinibispo <vini.bispo015@gmail.com>
2022-04-10 00:25:15 -0300

    test(users): add more expectations in users_system_spec

--- spec/system/users_system_spec.rb
+++ spec/system/users_system_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe 'Invoices', type: :system do
     visit '/users/new'
 
     fill_in User.human_attribute_name('email'), with: user.email
+
+    click_on button('users.create')
+
+    expect(page).to have_content(flash('users.create.success'))
+    expect(page).to have_current_path(invoices_path)
   end
 end

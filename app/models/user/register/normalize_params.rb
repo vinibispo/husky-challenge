8303947033091c7ed9e4ef50fa24e983bccd0fbb
vinibispo <vini.bispo015@gmail.com>
2022-04-10 10:29:13 -0300

    feat(normalize_params): add normalize_params for register for user

---
+++ app/models/user/register/normalize_params.rb
@@ -0,0 +1,17 @@
+# normailze_params for register for user
+class User::Register::NormalizeParams < Micro::Case
+  attribute :params
+  validates :params, kind: ActionController::Parameters
+
+  ValidateEmail = ->(email) { email =~ URI::MailTo::EMAIL_REGEXP }
+
+  def call!
+    return Failure :blank_email, result: { message: I18n.t('email.blank', scope: 'activerecord.errors.models.user.attributes') } if user_params[:email]
+  end
+
+  private
+
+  def user_params
+    ::User::Params.to_save(params)
+  end
+end

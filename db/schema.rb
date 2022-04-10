0906d713e17a6456ce074cf6d6826f251c139b9b
vinibispo <vini.bispo015@gmail.com>
2022-04-10 12:18:55 -0300

    feat(users): add index for email for performance management in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_09_195750) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_10_151647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_09_195750) do
     t.string "token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 

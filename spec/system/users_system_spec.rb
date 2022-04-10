746de100bb1d83bb8825c793698272c84bd002cc
vinibispo <vini.bispo015@gmail.com>
2022-04-10 01:23:43 -0300

    fix(user_spec): replace new for register on system spec user

--- spec/system/users_system_spec.rb
+++ spec/system/users_system_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 RSpec.describe 'Invoices', type: :system do
   it 'should create a user (generate access token)' do
     user = build(:user)
-    visit '/users/new'
+    visit '/users/register'
 
     fill_in User.human_attribute_name('email'), with: user.email
 

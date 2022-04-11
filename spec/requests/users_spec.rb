aa4747287dfbffaf90f7820058e3d99b505aa29d
vinibispo <vini.bispo015@gmail.com>
2022-04-11 01:12:18 -0300

    test(users): add test for errors validations

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,5 +11,43 @@ RSpec.describe '/users' do
 
       expect(response.body).to include(flash('users.create.success'))
     end
+
+    describe 'when has errors' do
+      it 'should redirect to current_page when has no key user' do
+        post users_path, params: { hello: "world" }
+
+        expect(response).to redirect_to(register_users_path)
+
+        follow_redirect!
+
+        expected = 'param is missing or the value is empty: user'
+
+        expect(response.body).to include(expected)
+      end
+
+      it 'should redirect to current_page when has blank email' do
+        post users_path, params: { user: { email: '' } }
+
+        expect(response).to redirect_to(register_users_path)
+
+        follow_redirect!
+
+        expected = 'blank'
+
+        expect(response.body).to include(expected)
+      end
+
+      it 'should redirect to current_page when has invalid email' do
+        post users_path, params: { user: { email: 'invalid-email' } }
+
+        expect(response).to redirect_to(register_users_path)
+
+        follow_redirect!
+
+        expected = 'invalid'
+
+      expect(response.body).to include(expected)
+      end
+    end
   end
 end

aab531ef374ac9bc8bc940a1bd317c2fc4388b10
vinibispo <vini.bispo015@gmail.com>
2022-04-11 00:37:17 -0300

    test(create_record): add test for validating failure in create_record

--- spec/models/user/register/create_record_spec.rb
+++ spec/models/user/register/create_record_spec.rb
@@ -24,4 +24,18 @@ RSpec.describe User::Register::CreateRecord do
     expect(result.success?).to be true
     expect(result.data).to eq(expected)
   end
+
+  it 'should return failure when setup a unique email twice' do
+    email = 'johndoe@husky.io'
+    create(:user, email:)
+
+    result = User::Register::CreateRecord.call(user: nil, email:)
+
+    expected = ['Email has already been taken']
+
+    expect(result.failure?).to be true
+    expect(result[:errors].map(&:full_message)).to eq(expected)
+
+    
+  end
 end

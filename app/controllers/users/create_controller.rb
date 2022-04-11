32540f5707938903dc74e30b4505f74377d1332a
vinibispo <vini.bispo015@gmail.com>
2022-04-11 01:11:58 -0300

    feat(user): convert controller user create in use cases

--- app/controllers/users/create_controller.rb
+++ app/controllers/users/create_controller.rb
@@ -1,21 +1,18 @@
 # create controller for users
 class Users::CreateController < ApplicationController
   def call
-    user = User.new(User::Params.to_save(params))
-    respond_to do |format|
-      which_screen_should_render(format, user)
-    end
+    ::User::Register::Flow
+      .call(params:)
+      .on_success { redirect_to root_path, notice: I18n.t("flash.users.create.success") }
+      .on_failure(:blank_email) { |error| redirect_error(error[:message]) }
+      .on_failure(:invalid_email) { |error| redirect_error(error[:message]) }
+      .on_failure(:parameter_missing) { |error| redirect_error(error[:message]) }
   end
 
   private
 
-  def which_screen_should_render(format, user)
-    if user.save
-      format.html { redirect_to invoices_url, notice: I18n.t("flash.users.create.success") }
-      format.json { render 'users/show', locals: { user: }, status: :created }
-    else
-      format.html { render 'users/new', locals: { user: }, status: :unprocessable_entity }
-      format.json { render json: user.errors, status: :unprocessable_entity }
-    end
+  def redirect_error(error)
+    flash[:notice] = error
+    redirect_to register_users_path
   end
 end

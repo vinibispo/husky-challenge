9e71348fd25b5cf386287343265a2882597c8867
vinibispo <vini.bispo015@gmail.com>
2022-04-12 08:30:48 -0300

    feat(user): add user_tokens as references of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  has_secure_token
+  has_many :user_tokens, inverse_of: :user, dependent: :destroy
   validates :email, presence: true, uniqueness: true
+  has_secure_token
 end

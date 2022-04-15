ba48e8109a2351936e7fb67f36cf878b762004af
vinibispo <vini.bispo015@gmail.com>
2022-04-15 13:46:49 -0300

    chore(rubocop): indenting new_controller for making rubocop happy

--- app/controllers/sessions/new_controller.rb
+++ app/controllers/sessions/new_controller.rb
@@ -1,6 +1,7 @@
+# new controller for sessions
 class Sessions::NewController < ApplicationController
   def call
-   result =  LoginLink.valid?(request.original_url)
+    result = LoginLink.valid?(request.original_url)
     if result
       session[:current_user_token] = params[:token]
       redirect_to invoices_path

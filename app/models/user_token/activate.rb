35e7c8c0d94e450550148cd8c8e5610372f1fb1f
vinibispo <vini.bispo015@gmail.com>
2022-04-15 09:27:54 -0300

    feat(acitvate): add use case for activate for user_token

---
+++ app/models/user_token/activate.rb
@@ -0,0 +1,12 @@
+class UserToken::Activate < Micro::Case
+  attribute :user
+  attribute :user_token
+  
+  validates :user, kind: User
+  validates :user_token, kind: UserToken
+
+  def call!
+    user_token.activate!
+    Success result: { user:, user_token: }
+  end
+end

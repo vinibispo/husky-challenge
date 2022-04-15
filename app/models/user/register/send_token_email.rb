6e338b023fc46b820cc23840bae79587aa0fadf9
vinibispo <vini.bispo015@gmail.com>
2022-04-15 13:13:40 -0300

    fix(send_token_email): add token param on send_token_email

--- app/models/user/register/send_token_email.rb
+++ app/models/user/register/send_token_email.rb
@@ -1,10 +1,12 @@
 # send token email for register for user
 class User::Register::SendTokenEmail < Micro::Case
   attribute :user
+  attribute :token
   attribute :mailer, default: ->(value) { value || UserMailer }
   validates :user, kind: User
+  validates :token, kind: String
   def call!
-    mailer.with(user:).send_token.deliver_later
+    mailer.with(user:, token:).send_token.deliver_later
     Success result: { user:, message: I18n.t("flash.users.create.success") }
   end
 end

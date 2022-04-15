d1830e28754ac8a8574e52d98ccfc1c3bcd8e84e
vinibispo <vini.bispo015@gmail.com>
2022-04-15 08:22:38 -0300

    feat(url_options): add initializer url_options for url_signature working well

---
+++ config/initializers/url_options.rb
@@ -0,0 +1,9 @@
+Rails.env.on(:development) do
+  Rails.application.routes.default_url_options = {
+    host: 'localhost',
+    protocol: :http,
+    port: 3_000
+  }
+
+  config.action_mailer.default_url_options = Rails.application.routes.default_url_options
+end

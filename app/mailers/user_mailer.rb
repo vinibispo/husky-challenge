ea52eb89770f457b3c36fea392d3b3586f31f955
vinibispo <vini.bispo015@gmail.com>
2022-04-15 13:16:40 -0300

    feat(user_mailer): add token and encrypted_url on user_mailer send_token

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,10 @@
 class UserMailer < ApplicationMailer
   def send_token
     user = params[:user]
+    token = params[:token]
+    url = LoginLink.create!(email: user.email, token:)
     mail(to: user.email, subject: "Token") do |format|
-      format.html { render 'user_mailer/send_token', locals: { user: } }
+      format.html { render 'user_mailer/send_token', locals: { user:, url:, token: } }
     end
   end
 end

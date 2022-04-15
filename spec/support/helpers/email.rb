c52ea7eab422151561b19458aabdcb707d240613
vinibispo <vini.bispo015@gmail.com>
2022-04-15 16:55:29 -0300

    feat(helpers): add email helper for click_email_link

---
+++ spec/support/helpers/email.rb
@@ -0,0 +1,8 @@
+module Helpers::Email
+  def click_email_link(index, mail = ActionMailer::Base.deliveries.last)
+    html = Nokogiri(mail.body.to_s)
+    link = html.css("a").to_a[index]
+
+    visit link["href"]
+  end
+end

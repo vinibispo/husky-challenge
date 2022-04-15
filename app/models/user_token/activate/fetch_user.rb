8fbdcb136cafb5db556b5c34786ac4d31cabb968
vinibispo <vini.bispo015@gmail.com>
2022-04-15 10:06:56 -0300

    feat(user_token): add implementation for activate flow, fetch_user and fetch_record (just for being saved)

---
+++ app/models/user_token/activate/fetch_user.rb
@@ -0,0 +1,12 @@
+class UserToken::Activate::FetchUser < Micro::Case
+  attribute :user_token
+  validates :user_token, kind: UserToken
+
+  def call!
+    user = user_token.user
+    return Failure :no_user, result: { message: 'There is no user asssociated' } if user.blank?
+
+    Success result: { user:, user_token: }
+    
+  end
+end

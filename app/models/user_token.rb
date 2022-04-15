64b85a324999058bd51826d4590204e0ab0767c1
vinibispo <vini.bispo015@gmail.com>
2022-04-15 09:17:58 -0300

    feat(user_token): add method activate!

--- app/models/user_token.rb
+++ app/models/user_token.rb
@@ -1,4 +1,11 @@
 class UserToken < ApplicationRecord
   has_secure_token
   belongs_to :user
+  scope :not_confirmed, -> { where(confirmed_at: nil) }
+  scope :not_revoked, -> { where(revoked_at: nil) }
+  scope :not_confirmed_and_not_revoked, -> { not_confirmed.not_revoked }
+
+  def activate!
+    update(confirmed_at: DateTime.now)
+  end
 end

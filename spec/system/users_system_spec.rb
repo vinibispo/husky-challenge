49db2565816b74e6d7b8aa64771617f7ba04d025
vinibispo <vini.bispo015@gmail.com>
2022-04-15 16:56:36 -0300

    test(users_system): add more tests for users_system

--- spec/system/users_system_spec.rb
+++ spec/system/users_system_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe 'Invoices', type: :system do
+RSpec.describe 'Users', type: :system do
   it 'should create a user (generate access token)' do
     user = build(:user)
     visit '/users/register'
@@ -10,5 +10,28 @@ RSpec.describe 'Invoices', type: :system do
 
     expect(page).to have_content(flash('users.create.success'))
     expect(page).to have_current_path(root_path)
+  end
+
+  it 'should login user' do
+    user = create(:user)
+    user_token = create(:user_token, user:, confirmed_at: DateTime.now)
+
+    visit '/sessions/new'
+
+    fill_in User.human_attribute_name('token'), with: user_token.token
+
+    click_on button('sessions.create')
+  end
+
+  it 'should login user directly' do
+    user = create(:user)
+    user_token = create(:user_token, user:)
+    UserMailer.with(user: user, token: user_token.token).send_token.deliver_now
+
+    click_email_link(0)
+
+    expect(page).to have_current_path(invoices_path)
+    expect(page).to have_content('Invoices')
+
   end
 end

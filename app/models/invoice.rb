4b32458b74c50fd9ec88fd86a99ad72807925daf
vinibispo <vini.bispo015@gmail.com>
2022-04-16 15:39:20 -0300

    fix(invoice): fix typo on invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,6 @@
 class Invoice < ApplicationRecord
   belongs_to :user, default: -> { Current.user }, inverse_of: :invoices
-  has_many_atttached :documents
+  has_many_attached :documents
   after_save do
     InvoiceMailer.with(invoice: self).created.deliver_now
   end

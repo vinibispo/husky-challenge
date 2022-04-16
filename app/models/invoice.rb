474559962969af49cc6426de5659816b04bda5c1
vinibispo <vini.bispo015@gmail.com>
2022-04-16 17:20:34 -0300

    feat(invoice): add validates in emails

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,5 @@
 class Invoice < ApplicationRecord
+  validates :emails, presence: true
   belongs_to :user, default: -> { Current.user }, inverse_of: :invoices
   has_many_attached :documents
-  after_save do
-    InvoiceMailer.with(invoice: self).created.deliver_now
-  end
 end

1f46a6cc982a8d14e03ad742b877787c5fbdbeb9
vinibispo <vini.bispo015@gmail.com>
2022-04-16 18:09:27 -0300

    feat(invoices): add New::Flow in action New

--- app/controllers/invoices/new_controller.rb
+++ app/controllers/invoices/new_controller.rb
@@ -1,9 +1,23 @@
 # new controller for invoices
 class Invoices::NewController < ApplicationController
   def call
-    return redirect_if_is_not_authenticated if not_authenticated_and_html?
+    ::Invoice::New::Flow.call(token: session[:current_user_token])
+                          .on_failure(:invalid_attributes) { redirect_no_token_error }
+                          .on_failure(:no_token) { |error| redirect_error(error[:message]) }
+                          .on_success do |result|
+        render 'invoices/new', locals: { invoice: result[:invoice] }
+      end
+  end
 
-    invoice = Invoice.new
-    render 'invoices/new', locals: { invoice: }
+  private
+  
+  def redirect_error(message)
+    redirect_to root_path
+    flash[:danger] = message
+  end
+
+  def redirect_no_token_error
+    redirect_to root_path
+    flash[:danger] = I18n.t('flash.user.unauthorized')
   end
 end

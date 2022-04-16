22ce8f5dea12e03fbcffc196bd69ce67d3d589de
vinibispo <vini.bispo015@gmail.com>
2022-04-16 11:16:18 -0300

    fix(user): destroy invoices if user is destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :user_tokens, inverse_of: :user, dependent: :destroy
-  has_many :invoices, inverse_of: :user
+  has_many :invoices, inverse_of: :user, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
 end

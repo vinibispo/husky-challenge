b7fe813775413b30e4a423dcf7813a6ff0e1e1bc
vinibispo <vini.bispo015@gmail.com>
2022-04-16 10:27:19 -0300

    feat(invoice): add current_user in invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ApplicationRecord
-  belongs_to :user, inverse_of: :invoices
+  belongs_to :user, default:-> { Current.user } , inverse_of: :invoices
   after_save do
     InvoiceMailer.with(invoice: self).created.deliver_now
   end

4d8619a13c705130553c266f79a8c343cd115f13
vinibispo <vini.bispo015@gmail.com>
2022-04-16 01:13:09 -0300

    feat(migration): add user to invoices on migration and in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_113203) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_16_033840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_113203) do
     t.string "emails"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_invoices_on_user_id"
   end
 
   create_table "user_tokens", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_113203) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "invoices", "users"
 end

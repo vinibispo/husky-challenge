79dffb44da344fbaef2fe16b6f0e902f463fca9c
vinibispo <vini.bispo015@gmail.com>
2022-04-16 01:13:57 -0300

    feat(user): add user to invoices and invoices to user

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,5 @@
 class Invoice < ApplicationRecord
+  belongs_to :user, inverse_of: :invoices
   after_save do
     InvoiceMailer.with(invoice: self).created.deliver_now
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :user_tokens, inverse_of: :user, dependent: :destroy
+  has_many :invoices, inverse_of: :user
+
   validates :email, presence: true, uniqueness: true
 end

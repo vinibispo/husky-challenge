11d3f8d73ba784dabd8ae591dc952fb047896372
vinibispo <vini.bispo015@gmail.com>
2022-04-17 11:51:11 -0300

    feat(invoice_mailer): remove responsability from mailer

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,11 +1,19 @@
 # mailer for invoice
 class InvoiceMailer < ApplicationMailer
   def created
+    Invoice::PrepareInvoiceEmail
+      .call(invoice: params[:invoice])
+      .on_failure { |result| logger(result) }
+      .on_success { |result| render_email(result) }
+  end
+
+  private
+
+  def render_email(result)
     invoice = params[:invoice]
+    attachments["invoice-#{invoice.id}-#{SecureRandom.uuid}.pdf"] = result[:attachment]
+    emails = result[:emails]
 
-    emails = invoice.emails.split(',')
-
-    attachments["invoice-#{invoice.id}-#{SecureRandom.uuid}.pdf"] = { mime_type: 'application/pdf', content: invoice.documents.last.blob.download }
     mail(to: emails, subject: "Invoice #{invoice.id} from #{invoice.customer_name}") do |format|
       format.html { render 'invoice_mailer/created', locals: { invoice: } }
     end

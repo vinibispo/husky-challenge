bab6dfa6eef9c24ae20a755ad2b19d3cc692e02d
vinibispo <vini.bispo015@gmail.com>
2022-04-17 16:39:40 -0300

    feat(destroy): add implementation for destroy for sessions

---
+++ app/controllers/sessions/destroy_controller.rb
@@ -0,0 +1,7 @@
+class Sessions::DestroyController < ApplicationController
+  def call
+    session.delete(:current_user_token)
+    flash[:success] = I18n.t('flash.sessions.destroy.success')
+    redirect_to root_path, status: :see_other
+  end
+end

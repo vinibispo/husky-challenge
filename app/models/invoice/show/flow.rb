97e27a13fa96fa5749617bfbe993f72ba33b174f
vinibispo <vini.bispo015@gmail.com>
2022-04-17 15:20:06 -0300

    feat(invoice): add invoice show flow implementation

---
+++ app/models/invoice/show/flow.rb
@@ -0,0 +1,25 @@
+# flow for show for invoices
+class Invoice::Show::Flow < Micro::Case
+  attribute :id
+  attribute :token
+  validates :id, kind: { of: [String, Numeric] }
+
+  AuthenticationFlow = Micro::Cases.flow([Invoice::GetToken, Invoice::GetAuthentication])
+  private_constant :AuthenticationFlow
+
+  def call!
+    AuthenticationFlow
+      .call(token:)
+    get_invoice
+
+  end
+
+  private 
+
+  def get_invoice
+    invoice = Invoice.find(id)
+    Success result: { invoice: }
+  rescue ActiveRecord::RecordNotFound => e
+    Failure :not_found, result: { message: e.message }
+  end
+end

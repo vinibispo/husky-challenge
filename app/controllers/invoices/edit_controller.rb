826b7ef9a91eba620b23939bc34f2a2dcad6f650
vinibispo <vini.bispo015@gmail.com>
2022-04-17 11:01:28 -0300

    fix(invoices): remove useless controllers on invoices

--- app/controllers/invoices/edit_controller.rb
+++
@@ -1,8 +0,0 @@
-# edit controller for invoices
-class Invoices::EditController < ApplicationController
-  def call
-
-    invoice = Invoice.find(params[:id])
-    render 'invoices/edit', locals: { invoice: }
-  end
-end

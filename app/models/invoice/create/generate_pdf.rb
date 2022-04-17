3e0a425a021605b7ac8baa6ce99f3b14aa5ee605
vinibispo <vini.bispo015@gmail.com>
2022-04-17 12:22:44 -0300

    feat(generate_pdf): add validation for invoice attribute and split generate_pdf in attach_pdf and create_pdf

--- app/models/invoice/create/generate_pdf.rb
+++ app/models/invoice/create/generate_pdf.rb
@@ -1,8 +1,9 @@
 # generate pdf step for create for invoice
 class Invoice::Create::GeneratePdf < Micro::Case
   attribute :invoice
+  validates :invoice, kind: Invoice
   def call!
-    generate_pdf
+    attach_pdf
     Success result: { invoice: }
   rescue StandardError => e
     Failure :pdf_error, result: { message: e.message }
@@ -10,15 +11,19 @@ class Invoice::Create::GeneratePdf < Micro::Case
 
   private
 
-  def generate_pdf
+  def attach_pdf
+    pdf = create_pdf
+    io = StringIO.new pdf.render
+    invoice.documents.attach(io:, filename: "invoice-#{invoice.id}-#{SecureRandom.uuid}.pdf")
+  end
+
+  def create_pdf
     pdf = ::Prawn::Document.new
     pdf.text "Invoice: ##{invoice.invoice_number}"
     pdf.text "Date: #{invoice.invoice_date&.strftime('%a, %-d %b %Y')}"
     pdf.text "From: #{invoice.customer_name}"
     pdf.text "Notes: #{invoice.customer_notes}"
     pdf.text "Total: #{invoice.total_amount_due}"
-    string_pdf = pdf.render
-    io = StringIO.new string_pdf
-    invoice.documents.attach(io:, filename: "invoice-#{invoice.id}-#{SecureRandom.uuid}.pdf")
+    pdf
   end
 end

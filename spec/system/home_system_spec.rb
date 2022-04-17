92c9b2ae81e68916c74ffadc4ad527acec640ddd
vinibispo <vini.bispo015@gmail.com>
2022-04-17 19:34:10 -0300

    test(home): add test for home verifying if user is authenticated

---
+++ spec/system/home_system_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe "Home", type: :system do
+  it 'should redirect to invoices if user is logged in' do
+    user = create(:user)
+    user_token = create(:user_token, user:, confirmed_at: DateTime.now)
+
+    sign_in_as(user_token.token)
+
+    visit '/'
+  expect(page).to have_current_path(invoices_path)
+  end
+end

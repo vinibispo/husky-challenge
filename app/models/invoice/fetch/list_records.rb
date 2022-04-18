8aa74740ee7a2cac36ab3ad3369a9ae1cecc680e
vinibispo <vini.bispo015@gmail.com>
2022-04-17 22:09:52 -0300

    feat(ransack): add ransack in controller, model and view

--- app/models/invoice/fetch/list_records.rb
+++ app/models/invoice/fetch/list_records.rb
@@ -1,9 +1,13 @@
 # list records step for fetch for invoice
 class Invoice::Fetch::ListRecords < Micro::Case
   attribute :user
+  attribute :params
   validates :user, kind: User
+  validates :params, kind: ActionController::Parameters
   def call!
-    invoices = user.invoices
-    Success result: { invoices: }
+    all_invoices = user.invoices
+    q = all_invoices.ransack(params[:q])
+    invoices = q.result.includes(:documents_attachments)
+    Success result: { invoices:, q: }
   end
 end

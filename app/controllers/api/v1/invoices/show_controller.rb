89cbad522723d89b3639c5b63cd8ac5d95499bf4
vinibispo <vini.bispo015@gmail.com>
2022-04-20 02:10:32 -0300

    feat(controller): add implementation for show_controller for api for invoices

---
+++ app/controllers/api/v1/invoices/show_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::Invoices::ShowController < ApplicationController
+  def call
+    authenticate_or_request_with_http_token do |token|
+    Invoice::Show::Authorized::Flow.call(token:, id: params[:id])
+      .on_failure(:no_token) { render_unauthorized }
+      .on_failure(:not_found) { render_not_found } 
+      .on_success { |result| render_invoice(result) }
+    end
+  end
+
+  private
+
+  def render_unauthorized
+    head :unauthorized
+  end
+
+  def render_not_found
+    head :not_found
+  end
+  def render_invoice(result)
+    render 'invoices/show', locals: { invoice: result[:invoice] }, formats: [:json]
+  end
+end

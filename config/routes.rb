186e72ba701e047d29d814e8d24de3698d9b2642
vinibispo <vini.bispo015@gmail.com>
2022-04-20 00:57:36 -0300

    feat(routes): add api routes and rswag routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   modular_routes do
     resources :invoices, only: %i[index new create show] do
       put :send_emails, on: :member
@@ -11,6 +13,14 @@ Rails.application.routes.draw do
       get :home, on: :collection
     end
     resources :sessions, only: %i[new create destroy]
+
+    namespace :api do
+      namespace :v1 do
+        resources :invoices, only: %i[index create show] do
+          put :send_emails, on: :member
+        end
+      end
+    end
   end
 
   root 'publics/home#call'

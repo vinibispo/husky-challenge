d2f00dc206574e784cfebfb796fd7dc352e49f2c
vinibispo <vini.bispo015@gmail.com>
2022-04-20 01:39:19 -0300

    test(api_v1_invoices_create_spec): add test for api/v1/invoices/create_controller

---
+++ spec/requests/api/v1/invoices/create_spec.rb
@@ -0,0 +1,67 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/invoices/create', type: :request do
+
+  path '/api/v1/invoices' do
+
+    post('call create') do
+      tags "Invoices"
+      consumes "application/json"
+      parameter name: :params, in: :body, schema: {
+        type: :object,
+        properties: {
+          invoice: { type: :object, properties: {
+
+            invoice_number: { type: :integer },
+            invoice_date: { type: :string },
+            customer_name: { type: :string },
+            customer_name: { type: :string },
+            total_amount_due: { type: :integer },
+            emails: { type: :string }
+          } }
+        },
+        required: ['emails']
+      }
+      response(200, 'successful') do
+        let(:params) { { invoice: { emails: "vini@husky.io,vini@google.com"} } }
+
+        let(:user) { create(:user) }
+        let(:user_token)  { create(:user_token, user:, confirmed_at: DateTime.now) }
+        security [Bearer: []]
+
+        let(:Authorization) { "Bearer #{user_token.token}" }
+
+        # after do |example|
+        #   example.metadata[:response][:content] = {
+        #     'application/json' => {
+        #       example: JSON.parse(response.body, symbolize_names: true)
+        #     }
+        #   }
+        # end
+        run_test!
+      end
+
+      response(422, 'unprocessable entity') do
+        
+        let(:params) { { invoice: { emails: "" } } }
+
+        let(:user) { create(:user) }
+        let(:user_token)  { create(:user_token, user:, confirmed_at: DateTime.now) }
+        security [Bearer: []]
+
+        let(:Authorization) { "Bearer #{user_token.token}" }
+        run_test!
+      end
+
+      response(401, 'unauthorized') do
+        let(:params) { { invoice: { emails: "vini@husky.io,vini@google.com" } } }
+        security [Bearer: []]
+
+        let(:Authorization) { "Bearer t" }
+        run_test!
+    
+
+      end
+    end
+  end
+end
